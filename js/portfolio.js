6e29d2b59ab60a0e6f03ee5369b3d8e2fff222a8
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-11-13 16:11:31 -0800

    Edited js portfolio view more buton

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,71 +9,76 @@ const IMAGE_MOBILE = 4;
 const IMAGE_TABLET = 6;
 const IMAGE_DESKTOP = 9;
 
-handleImages(initialWidth);
+handleImages(initialWidth, "makeup");
+handleImages(initialWidth, "hairstyle");
 
 function handleTab(event) {
-  const target = event.target;
-  const targetText = target.textContent.toLowerCase();
-  const tab = target.parentElement;
+    const target = event.target;
+    const targetText = target.textContent.toLowerCase();
+    const tab = target.parentElement;
 
-  tab.classList.add("on");
-  if(tab.previousElementSibling !== null) {
-    tab.previousElementSibling.classList.remove("on");
-  } else if(tab.nextElementSibling !== null) {
-    tab.nextElementSibling.classList.remove("on");
-  }
-  tabPanels.forEach(tabPanel => {
-    tabPanel.classList.add("hide");
-    if (tabPanel.classList.contains(targetText)) {
-      tabPanel.classList.remove("hide");
+    tab.classList.add("on");
+    if(tab.previousElementSibling !== null) {
+        tab.previousElementSibling.classList.remove("on");
+    } else if(tab.nextElementSibling !== null) {
+        tab.nextElementSibling.classList.remove("on");
     }
-  })
+    tabPanels.forEach(tabPanel => {
+        tabPanel.classList.add("hide");
+        if (tabPanel.classList.contains(targetText)) {
+        tabPanel.classList.remove("hide");
+        }
+    })
 }
 
-function handleImages(width) {
-  if(width > 1000) {
-    showImages(IMAGE_DESKTOP);
-  } else if(width > 600) {
-    showImages(IMAGE_TABLET);
-  } else {
-    showImages(IMAGE_MOBILE);
-  }
+function handleImages(width, panelName) {
+    if(width > 1000) {
+        showImages(IMAGE_DESKTOP, panelName);
+    } else if(width > 600) {
+        showImages(IMAGE_TABLET, panelName);
+    } else {
+        showImages(IMAGE_MOBILE, panelName);
+    }
 }
 
-function showImages(count) {
-  makeupLists.forEach((makeupList, index) => {
-    if (index < count) {
-      makeupList.classList.add("show");
+function showImages(count, panel) {
+    if(panel == "makeup"){
+        makeupLists.forEach((makeupList, index) => {
+            if (index < count) {
+                makeupList.classList.add("show");
+            } else {
+                makeupList.classList.remove("show");
+            }
+        })
     } else {
-      makeupList.classList.remove("show");
+        hairstyleLists.forEach((hairstyleList, index) => {
+            if (index < count) {
+                hairstyleList.classList.add("show");
+            } else {
+                hairstyleList.classList.remove("show");
+            }
+        })
     }
-  })
-  hairstyleLists.forEach((hairstyleList, index) => {
-    if (index < count) {
-      hairstyleList.classList.add("show");
-    } else {
-      hairstyleList.classList.remove("show");
-    }
-  })
 }
 
 function handleViewMore(event) {
-  const target = event.target;
-  const panel = target.parentElement;
-  const windowWidth = window.innerWidth;
+    const target = event.target;
+    const panel = target.parentElement;
+    const panelName = panel.className;
+    const windowWidth = window.innerWidth;
 
-  showImages(windowWidth);
+    showImages(windowWidth, panelName);
 }
 
 btnTabs.forEach(btnTab => {
-  btnTab.addEventListener("click", handleTab);
+    btnTab.addEventListener("click", handleTab);
 })
 
 btnMores.forEach(btnMore => {
-  btnMore.addEventListener("click", handleViewMore)
+    btnMore.addEventListener("click", handleViewMore)
 })
 
 window.addEventListener("resize", function (event) {
-  handleImages(event.target.innerWidth);
+    handleImages(event.target.innerWidth);
 });
 
